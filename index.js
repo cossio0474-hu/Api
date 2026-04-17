8c018d05f9cd68fccc925afaec618d5671c6ac9d
Juan Cossio <cossio0474@gmail.com>
2026-04-17 09:39:28 -0500

    Primer endpoint

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const express = reqire("expres")
+const express = require("express")
+process.loadEnvFile()
+
+
+const app = express()
+
+const PORT = process.env.PORT
+/*
+const funcion = (A) => {
+
+}
+*/
+
+app.get("/", (req, res) =>{
+    res.send("Hello World!")
+})
+
+app.listen(PORT, () => {
+    console.log("Corriendo")
+} )
+
